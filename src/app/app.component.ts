47f7e9c28815e3676fe4241d3c6de6d6af30cdb2
Eve-Sama <948832626@qq.com>
2023-02-15 18:27:05 +0800

    feat: add x6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Graph } from '@antv/x6';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'x6-angular-shape';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('container') container: ElementRef;
+
+  ngAfterViewInit(): void {
+    new Graph({
+      container: this.container.nativeElement,
+      width: 800,
+      height: 600,
+      background: {
+        color: '#F2F7FA',
+      },
+    });
+  }
 }
